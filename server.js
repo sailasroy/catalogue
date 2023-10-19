4bf9743aeede3bf5c363acefc0243ef2bca5a8c1
sailasroy <sailasroy123@gmail.com>
2023-10-19 17:18:40 +0530

    adding health as part of new client requirement

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OK',
+        app: 'OKKK',
         mongo: mongoConnected
     };
     res.json(stat);
